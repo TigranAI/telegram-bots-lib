dba67c4c53c34e4f7fc87087228260e2e7271f59
Tigran Shaginyan <68780335+TigranAI@users.noreply.github.com>
2024-08-07 20:22:08 +0300

    Поправил условие конфига

--- src/main/kotlin/ru/tigran/telegram/bots/configuration/polling/TelegramPollerConfiguration.kt
+++ src/main/kotlin/ru/tigran/telegram/bots/configuration/polling/TelegramPollerConfiguration.kt
@@ -1,11 +1,11 @@
 package ru.tigran.telegram.bots.configuration.polling
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
-@ConditionalOnProperty("telegram.poller.enabled")
+@ConditionalOnExpression("\${telegram.poller.enabled:true}")
 @Configuration
 @ComponentScan("ru.tigran.telegram.bots.polling")
 @EnableConfigurationProperties(TelegramPollerProperties::class)
